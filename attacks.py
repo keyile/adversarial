06c32fb0eedf8f4394829db0d32c550a63526e7e
sourcerunner <xinfeiyang-2008@163.com>
2019-03-14 18:44:43 +0800

    Add one step target method

--- attacks.py
+++ attacks.py
@@ -59,3 +59,43 @@ def bim_attack(model, data, target, epsilon=0.1, nb_iter=3, alpha=0.1):
 
     # Return the perturbed image
     return data + eta
+
+
+def onestep_attack(model, data, target, epsilon=0.25):
+    r"""The One-step target class methods
+    """
+
+    # Set requires_grad attribute of tensor. Important for Attack
+    data.requires_grad = True
+
+    # Forward pass the data through the model
+    output = model(data)
+
+    if not target is None:
+        # Choose the least-likely class
+        with torch.no_grad():
+            target = output.argmin(dim=1)
+
+    # Calculate the loss
+    loss = F.nll_loss(output, target)
+
+    # Calculate gradients of model in backward pass
+    loss.backward()
+
+    # Collect datagrad
+    data_grad = data.grad.data
+
+    # Stop recording gradients
+    data.requires_grad = False
+
+    # Collect the element-wise sign of the data gradient
+    sign_data_grad = data_grad.sign()
+
+    # Create the perturbed image by adjusting each pixel of the input image
+    perturbed_image = data - epsilon*sign_data_grad
+
+    # Adding clipping to maintain [0,1] range
+    perturbed_image = torch.clamp(perturbed_image, 0, 1)
+
+    # Return the perturbed image
+    return perturbed_image
