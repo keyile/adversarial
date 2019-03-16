bbd0eaa3815b3c3410d05cb25b2559ba152b0668
sourcerunner <xinfeiyang-2008@163.com>
2019-03-16 12:46:40 +0800

    support argument parsing

--- mnist_fgsm.py
+++ mnist_fgsm.py
@@ -1,7 +1,7 @@
 r"""
 The FGSM attacking tutorial on MNIST dataset.
 """
-
+import argparse
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
@@ -40,54 +40,67 @@ def model_eval(model, test_loader, device, attack, **attack_params):
 
 
 def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='Adversarial MNIST Example')
+    parser.add_argument('--use-pretrained', action='store_true', default=False,
+                        help='uses the pretrained model')
+    parser.add_argument('--no-adversarial-training', action='store_true', default=False,
+                        help='takes the adversarial training process')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    args = parser.parse_args()
 
     # Define what device we are using
-    print("CUDA Available: ", torch.cuda.is_available())
-    device = torch.device("cuda" if (torch.cuda.is_available()) else "cpu")
+    use_cuda = torch.cuda.is_available()
+    # print("CUDA Available: ", use_cuda)
+    device = torch.device("cuda" if use_cuda else "cpu")
 
     # MNIST Test dataset and dataloader declaration
-    kwargs = {'num_workers': 1, 'pin_memory': True} if torch.cuda.is_available() else {}
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=True, download=True, transform=transforms.Compose([
             transforms.ToTensor()
         ])),
-        batch_size=64, shuffle=True, **kwargs)
+        batch_size=args.batch_size, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=False, transform=transforms.Compose([
             transforms.ToTensor()
         ])),
-        batch_size=128, shuffle=True, **kwargs)
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
     # Initialize the network
     model = LeNet().to(device)
+    if args.use_pretrained:
+        # Load the pretrained model
+        model.load_state_dict(torch.load('resources/lenet_mnist_model.bin', map_location='cpu'))
+    else:
+        # Train an MNIST model
+        model_train(model, train_loader, device, 10, noop_attack)
 
-    # Load the pretrained model
-    # model.load_state_dict(torch.load(MODEL_PATH, map_location='cpu'))
-
-    # Train an MNIST model
-    model_train(model, train_loader, device, 10, noop_attack)
-
-    # Evaluate the accuracy of the MNIST model on benign examples
+    # Evaluate the accuracy of the MNIST model on clean examples
     accuracy = model_eval(model, test_loader, device, noop_attack)
-    print('Test accuracy on benign examples: ' + str(accuracy))
+    print('Test accuracy on clean examples: ' + str(accuracy))
 
     # Evaluate the accuracy of the MNIST model on adversarial examples
     accuracy = model_eval(model, test_loader, device, fgsm_attack)
     print('Test accuracy on adversarial examples: ' + str(accuracy))
 
-    print("Repeating the process, using adversarial training")
-    # Perform adversarial training
-    model_train(model, train_loader, device, 10, fgsm_attack)
+    if not args.no_adversarial_training:
+        print("Repeating the process, using adversarial training")
+        # Perform adversarial training
+        model_train(model, train_loader, device, 10, fgsm_attack)
 
-    # Evaluate the accuracy of the adversarialy trained MNIST model on
-    # benign examples
-    accuracy = model_eval(model, test_loader, device, noop_attack)
-    print('Test accuracy on benign examples: ' + str(accuracy))
+        # Evaluate the accuracy of the adversarially trained MNIST model on
+        # clean examples
+        accuracy = model_eval(model, test_loader, device, noop_attack)
+        print('Test accuracy on clean examples: ' + str(accuracy))
 
-    # Evaluate the accuracy of the adversarially trained MNIST model on
-    # adversarial examples
-    accuracy_adv = model_eval(model, test_loader, device, fgsm_attack)
-    print('Test accuracy on adversarial examples: ' + str(accuracy_adv))
+        # Evaluate the accuracy of the adversarially trained MNIST model on
+        # adversarial examples
+        accuracy_adv = model_eval(model, test_loader, device, fgsm_attack)
+        print('Test accuracy on adversarial examples: ' + str(accuracy_adv))
 
 
 if __name__ == '__main__':
